f2021b0d3dd182528a11a3184f1b707ecf65bf7a
김효환 <gyghks1017@gmail.com>
2024-11-06 15:17:18 +0900

    [Design] : Frist Page LayOut

--- homepage/src/pages/First.jsx
+++ homepage/src/pages/First.jsx
@@ -1,7 +1,22 @@
+import NurimLogo from '../assets/firstpage/Logo.png';
+
 const First = () => {
   return (
     <div className="bg-[#FFECDF] w-full h-full min-h-screen">
-      <h1>ddd</h1>
+      {/* 헤더의 배경색을 전체 너비로 채우기 */}
+      <header className="bg-white w-full">
+        <div className="p-5 flex items-center justify-between max-w-[70%] mx-auto">
+          <img src={NurimLogo} alt="Nurim Logo" />
+
+          <div className="flex space-x-[30px] text-black">
+            <div>누림이란?</div>
+            <div>앞으로의 누림</div>
+            <div>이용약관</div>
+          </div>
+        </div>
+      </header>
+
+      <div className="border border-black w-[70%] h-[80vh] mx-auto my-8"></div>
     </div>
   );
 };
--- homepage/tailwind.config.js
+++ homepage/tailwind.config.js
@@ -6,7 +6,11 @@ export default {
     './src/**/*.{js,jsx,ts,tsx}', // src 폴더 내 모든 JavaScript 및 JSX 파일을 포함
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        jua: ['Jua', 'sans-serif'], // 로컬 폰트 Jua 추가
+      },
+    },
   },
   plugins: [],
 };
