f3e6dfb0e127fd18f5b644904e40b84709e0045d
jangkiwoong <wkdrldnd99@gmail.com>
2024-11-12 15:37:47 +0900

    [Feat] : 버튼 클릭시 사이드바 생성 기능 추가

--- homepage/src/components/Header.jsx
+++ homepage/src/components/Header.jsx
@@ -1,20 +1,39 @@
-import styled from 'styled-components';
-import NurimLogo from '../assets/firstpage/Logo.png';
+import styled from "styled-components";
+import NurimLogo from "../assets/firstpage/Logo.png";
+import { useState } from "react";
 
 const Header = () => {
-    return(
-        <HeaderContainer>
-            <HeaderContent>
-            <Logo src={NurimLogo} alt="Nurim Logo" />
-            <Menu>
-                <MenuItem>누림이란?</MenuItem>
-                <MenuItem>앞으로의 누림</MenuItem>
-                <MenuItem>이용약관</MenuItem>
-            </Menu>
-            </HeaderContent>
-        </HeaderContainer>
-    );
+  const [isToggleMenu, setToggleMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setToggleMenu(!isToggleMenu);
+  };
+
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <Logo src={NurimLogo} alt="Nurim Logo" />
+        <Menu>
+          <MenuItem>누림이란?</MenuItem>
+          <MenuItem>앞으로의 누림</MenuItem>
+          <MenuItem>이용약관</MenuItem>
+        </Menu>
+        <MobileMenu onClick={toggleMenu}>
+          <Bar />
+          <Bar />
+          <Bar />
+        </MobileMenu>
+      </HeaderContent>
+      <MobileSidebar isOpen={isToggleMenu}>
+        <CloseButton onClick={toggleMenu}>×</CloseButton>
+        <MobileMenuItem>누림이란?</MobileMenuItem>
+        <MobileMenuItem>앞으로의 누림</MobileMenuItem>
+        <MobileMenuItem>이용약관</MobileMenuItem>
+      </MobileSidebar>
+    </HeaderContainer>
+  );
 };
+
 export default Header;
 
 const HeaderContainer = styled.header`
@@ -46,8 +65,67 @@ const Menu = styled.div`
   display: flex;
   gap: 30px;
   color: black;
+  @media (max-width: 875px) {
+    display: none;
+  }
 `;
 
 const MenuItem = styled.div`
+  cursor: pointer;
+`;
+
+const MobileMenu = styled.div`
+  display: none;
+  @media (max-width: 875px) {
+    display: flex;
+    cursor: pointer;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 3px;
+  }
+
+  border: 2px solid black;
+  border-radius: 5px;
+  width: 35px;
+  height: 25px;
+`;
+const Bar = styled.div`
+  width: 25px;
+  height: 3px;
+  background-color: black;
+  border-radius: 5px;
+`;
+
+const MobileSidebar = styled.div`
+  position: fixed;
+  top: 0;
+  right: ${(props) => (props.isOpen ? "0" : "-100%")};
+  width: 50%;
+  height: 100%;
+  background-color: white;
+  box-shadow: 2px 0 5px rgba(0, 0, 0, 0.2);
+  transition: left 0.3s ease;
+  z-index: 9999;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  @media (min-width: 875px) {
+    display: none;
+  }
+`;
+
+const CloseButton = styled.div`
+  align-self: flex-start;
+  font-size: 24px;
+  cursor: pointer;
+  margin-bottom: 20px;
+  color:black;
+`;
+
+const MobileMenuItem = styled.div`
+  color: black;
+  margin: 10px 0;
   cursor: pointer;
 `;
