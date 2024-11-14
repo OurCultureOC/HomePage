1591b99e15038133f519a76e51e3557d963fe9d5
jangkiwoong <wkdrldnd99@gmail.com>
2024-11-14 23:21:12 +0900

    [Feat] : first 페이지 테블릿 반응형 추가

--- homepage/src/components/MainIntro.jsx
+++ homepage/src/components/MainIntro.jsx
@@ -1,42 +1,46 @@
-import MainImage from '../assets/firstpage/image.png';
-import GoogleBtnIcon from '../assets/firstpage/GoogleStoreIcon.png';
-import AppStoreBtn from '../assets/firstpage/AppStoreIcon.png';
-import Button from './Button';
-import Circle from '../assets/firstpage/Circle.png'; // 동그라미 이미지
-import Wavve from '../assets/firstpage/Wave.png'; // 물결 이미지
+import MainImage from "../assets/firstpage/image.png";
+import GoogleBtnIcon from "../assets/firstpage/GoogleStoreIcon.png";
+import AppStoreBtn from "../assets/firstpage/AppStoreIcon.png";
+import Button from "./Button";
+import Circle from "../assets/firstpage/Circle.png"; // 동그라미 이미지
+import Wavve from "../assets/firstpage/Wave.png"; // 물결 이미지
 
 const MainIntro = () => {
   return (
-    <div className="w-full h-full flex text-black">
+    <div className="w-full h-full flex text-black flex-wrap">
       <div className="w-1/2 flex flex-col justify-center items-start">
-        <div className="mb-4">탁주부터 전통소주까지</div>
+        <div className="mb-4 text-base sm:text-2xl md:text-base lg:text-base">탁주부터 전통소주까지</div>
 
-        <div className="flex text-7xl font-jua font-bold text-black mb-1 relative">
+        <div className="flex lg:text-7xl md:text-5xl font-jua font-bold text-black mb-1 relative">
           <div className="mr-3 relative z-10">
             전통주
             <img
               src={Wavve}
               alt="Wave"
-              className="absolute top-0 left-[-10px] w-30 h-6 mt-12 ml-3 z-[-100]"
+              className="absolute top-0 left-[-10px] w-30 h-6 mt-12 ml-3 z-[-100] 
+                          md:w-25 md:left-[-15px] md:top-[-20px]
+                          "
             />
           </div>
-          <div className="relative z-10">
+          <div className="relative z-10 ">
             한 잔
             <img
               src={Circle}
               alt="Circle"
-              className="absolute top-0 left-2 w-40 h-20 -mt-2 -ml-2 z-[-100]"
+              className="absolute top-0 left-2 w-40 h-20 -mt-2 -ml-2 z-[-100] 
+                          md:w-35 md:h-14
+                          "
             />
           </div>
         </div>
 
-        <div className="text-7xl font-jua font-bold text-black mb-4">
+        <div className="text-7xl md:text-5xl font-jua font-bold text-black">
           생각 날 때
         </div>
-        <div className="text-xl mb-4">누림에서 가장 가까운 전통주점을</div>
-        <div className="text-xl">찾을 수 있어요.</div>
+        <div className="text-xl md:text-lg mb-4 md:mb-0">누림에서 가장 가까운 전통주점을</div>
+        <div className="text-xl md:text-lg">찾을 수 있어요.</div>
 
-        <div className="flex space-x-4 mt-4">
+        <div className="flex flex-wrap gap-3 mt-4">
           <Button
             icons={GoogleBtnIcon}
             text="Google Store"
@@ -57,11 +61,7 @@ const MainIntro = () => {
       </div>
 
       <div className="w-1/2 flex justify-center items-center">
-        <img
-          src={MainImage}
-          alt="Main"
-          className="w-485 h-485 rounded-full object-cover"
-        />
+        <img src={MainImage} alt="Main" className="rounded-full object-cover" />
       </div>
     </div>
   );
